37f1a1fd850ac62cd48f344fda881852f88f7b8c
zoeysann <safarovazahra1@gmail.com>
2024-09-20 10:31:16 +0400

    push

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 from .models import idk_name
 
 # Create your views here.
-def item_list(request):
+def home(request):
+    print('AAAAA')
     items = idk_name.objects.all()  # Retrieve all items from the database
+    print('5')
     context = {
         'items': items
     }
+    print(context)
     return render(request, 'vocabulary/index.html', context)
 
-def home(request):
-    return render(request, 'vocabulary/index.html')
+# def home(request):
+#     return render(request, 'vocabulary/index.html')
 
 def info(requst):
     return HttpResponse("Hello there! This's info page.")
